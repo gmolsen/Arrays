e6bfb92f7b7167522f1b4622d6a5db28464cdf04
Greg <gmolsenvideo@gmail.com>
2017-09-12 00:40:01 -0400

    Arrays and stuff

---
+++ ArrayLibrary/LearningArrays.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayLibrary
+{
+	public class LearningArrays {
+		public static string[] Students = { "a", "b", "c", "d", "e" };
+
+		public static int[] TestScores = { 95, 87, 99, 92 };
+
+		public static int[] Frames = { 23, 8, 23, 15, 11, 10, 3, 12, 15, 13 };
+
+		// multi-dimensional arrays
+		public static int[,] BowlingScores = new int[,] {
+			{23, 8, 23, 15, 11, 10, 3, 12, 15, 13 }, //first game
+			{8, 12, 14, 30,  0, 12,  6, 15,  16,  0}, //second game
+			{27, 18,  5, 24, 11,  17,  0, 19,  6, 2} //third game
+	};
+
+		public static List<int> GradingScores = new List<int>();
+
+		public static List<int> HighTemps = new List<int>();
+
+
+	}
+}
+// { 23, 8, 23, 15, 11, 10, 3, 12, 15, 13}
---
+++ TestArrayLibrary/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArrayLibrary;
+
+namespace TestArrayLibrary {
+	class Program {
+
+		void Run() {
+			Bowling();
+			var aStudent = LearningArrays.Students[0];
+			var score = LearningArrays.TestScores[0];
+			Console.WriteLine($"The fourth student in the array is {aStudent}, and their test score is {score}");
+			// iterating through all the items in the array
+			var Total = 0;
+			// always use .Count() method for indexing arrays
+			for (var idx = 0; idx < LearningArrays.TestScores.Count(); idx++) {
+				Total += LearningArrays.TestScores[idx];
+			}
+			// Total is casted as a double to display the true average
+			var Average = ((double)Total) / LearningArrays.TestScores.Count();
+			Console.WriteLine($"The sum of the scores is {Total} and the average is {Average}");
+		}
+
+		void Bowling() {
+			//var Total = 0;
+			//	for (var idx = 0; idx < LearningArrays.BowlingGame.Count(); idx++) {
+			//		Total += LearningArrays.BowlingGame[idx];
+			// shorthand version of this iteration below	}
+			//foreach (var score in LearningArrays.BowlingGame) {
+			//	Total += score;
+			//}
+			//var average = ((double)Total) / LearningArrays.BowlingGame.Count();
+			//Console.WriteLine($"The total bowling score is {Total}");
+			//Console.WriteLine($"The average bowling score is {Total / LearningArrays.BowlingGame.Count()}");
+			var Total = 0;
+			// shorthand version of iteration performed above by for loop
+			for (var idx = 0; idx < LearningArrays.Frames.Count(); idx++) {
+				Total += LearningArrays.Frames[idx];
+			}
+			Console.WriteLine($"The total bowling score is {Total}");
+			Console.WriteLine($"The average bowling score is {Total / LearningArrays.Frames.Count()}");
+			// multi-dimensional arrays
+			Console.WriteLine($"Bowling score 1, 6 is {LearningArrays.BowlingScores[1, 6]}");
+
+			LearningArrays.GradingScores.Add(94);
+			LearningArrays.GradingScores.Add(97);
+			LearningArrays.GradingScores.Add(89);
+			LearningArrays.GradingScores.Add(93);
+			LearningArrays.GradingScores.Add(92);
+			LearningArrays.GradingScores.Remove(97);
+			var idxGrades = LearningArrays.GradingScores.IndexOf(92);
+			LearningArrays.GradingScores[idxGrades] = 65;
+			Console.WriteLine($"There are {LearningArrays.GradingScores.Count()} items in the list.");
+			foreach (var score in LearningArrays.GradingScores) {
+				Console.WriteLine($"A score is {score}");
+			}
+
+			LearningArrays.HighTemps.Add(65);
+			LearningArrays.HighTemps.Add(63);
+			LearningArrays.HighTemps.Add(81);
+			LearningArrays.HighTemps.Add(86);
+			LearningArrays.HighTemps.Add(77);
+			LearningArrays.HighTemps.Add(69);
+			LearningArrays.HighTemps.Add(71);
+			LearningArrays.HighTemps.Add(76);
+			LearningArrays.HighTemps.Add(74);
+			LearningArrays.HighTemps.Add(74);
+			LearningArrays.HighTemps.Add(72);
+			Console.WriteLine($"The highest temperature is {LearningArrays.HighTemps.Max()}");
+			Console.WriteLine($"The avg high temperature is {LearningArrays.HighTemps.Average()}");
+
+			
+			/*storing an array
+			var scores = {1,2,3}
+			var scores = new int [10] -- better if u know exactly how many items will be in your array
+			var scores = new List<int>(); --defines a dynamic array list which can be changed
+			BowlingGame[2] = 30; ???
+			*/
+
+
+		}
+		static void Main(string[] args) {
+			new Program().Run();
+		}
+	}
+}
